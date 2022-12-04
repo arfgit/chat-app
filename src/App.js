f9ea3b2fd2c6aa29323d9e60102726fd204752ff
Anthony Feliz <47192556+arfgit@users.noreply.github.com>
2022-12-04 18:41:06 -0500

    app setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./features/home/Home";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
